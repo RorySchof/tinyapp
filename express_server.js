62d7fb0423bdbaee2b6375aa52c6396cdc0a891e
RorySchof <roryschofield@gmail.com>
2023-03-20 13:47:11 -0400

    Added partials

--- express_server.js
+++ express_server.js
@@ -1,12 +1,13 @@
 const express = require("express");
-
-
 const app = express();
 const PORT = 8080; // default port 8080
 
-
-
 app.set("view engine", "ejs");
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
@@ -28,4 +29,14 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: "wwww.google.com" };
+  res.render("urls_show", templateVars);
+});
+
+
+
+
 
