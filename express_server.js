f3895d1d11a48814aa6edad901fc026056bcdd4e
RorySchof <roryschofield@gmail.com>
2023-03-21 15:38:25 -0400

    Finished URL shortener 1

--- express_server.js
+++ express_server.js
@@ -2,7 +2,32 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+function generateRandomString(length) {
+  const characters = "abcdefghijklmnopqrstuvwxyz";
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters[randomIndex]
+}
+  return result;
+}
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
@@ -17,26 +42,15 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
-
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
-
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: "wwww.google.com" };
   res.render("urls_show", templateVars);
 });
 
-
-
-
-
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
