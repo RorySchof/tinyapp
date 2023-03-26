08e7be7191462855cbf64574edff14ff9c318b67
RorySchof <roryschofield@gmail.com>
2023-03-26 13:06:44 -0400

    Refactored the login route

--- express_server.js
+++ express_server.js
@@ -1,78 +1,103 @@
-const { request } = require("express");
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser');
+const { generateRandomString } = require('./utils');
 const cookieParser = require('cookie-parser');
 const PORT = 8080; // default port 8080
 
+app.use(cookieParser('some-secret-key'));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
-//app.use(express.cookieParser());
-
-
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-function generateRandomString(length) {
-  const characters = "abcdefghijklmnopqrstuvwxyz";
-  let result = '';
-  for (let i = 0; i < length; i++) {
-    const randomIndex = Math.floor(Math.random() * characters.length);
-    result += characters[randomIndex]
+const usersDatabase = {};
+
+// Helper function to find a user by email
+const getUserByEmail = function(email) {
+  for (const userId in usersDatabase) {
+    const user = usersDatabase[userId];
+    if (user.email === email) {
+      return user;
+    }
   }
-  return result;
-}
+  return null;
+};
 
-app.post("/login", (req, res) => {
-  console.log(req.body.username)
-  res.cookie("username", req.body.username)
-  res.redirect('/urls')
-})
+// ROUTING
 
+app.get('/register', (req, res) => {
+  res.render('register');
+});
 
-app.get("/u/:id", (req, res) => {
-  const shortUrl = req.params.id
-  const longURL = urlDatabase[shortUrl]
-  res.redirect(longURL);
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+
+  // check if email or password are empty
+  if (!email || !password) {
+    res.status(400).send('GIVE US YOUR EMAIL AND PASSWORD!!!');
+    return;
+  }
+
+  const user = getUserByEmail(email);
+
+  // check if user exists and password is correct
+  if (!user) {
+    res.status(403).send('WE CANT FIND YOUR EMAIL');
+    return;
+  }
+
+  if (user.password !== password) {
+    res.status(403).send('PASSWORD IS WRONG. BE BETTER');
+    return;
+  }
+
+  // set user_id cookie with user id
+  res.cookie('user_id', user.id, { signed: true });
+
+  res.redirect('/urls');
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("user_id", {path: "/"});
+  res.redirect('/login');
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = {
+    user_id: req.signedCookies.user_id,
+    urls: urlDatabase
+  };
+  res.render("urls_index", templateVars);
 });
 
 app.post("/urls", (req, res) => {
   const longURL = req.body.longURL; // Log the POST request body to the console
   const id = generateRandomString(6);
-  urlDatabase[id] = longURL
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  urlDatabase[id] = longURL;
+  res.redirect(`/urls/${id}`); // Redirect to newly created short URL
 });
+
 app.post("/urls/:id/delete", (req, res) => {
-  const shortUrl = req.params.id
-  const longURL = delete urlDatabase[shortUrl]
-  res.redirect('/urls')
+  const shortUrl = req.params.id;
+  delete urlDatabase[shortUrl];
+  res.redirect('/urls');
 });
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: "wwww.google.com" };
+  const templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
